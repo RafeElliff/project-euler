7a9f2e70ec14a1921c6339c2a0ac74651768d835
RafeElliff <rafe.elliff@westminster.org.uk>
2024-07-05 13:55:28 +0100

    Problem 4

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-import math
-initial_num = 600851475143
-new_num = initial_num
-sq_root_of_initial = int(round(math.sqrt(initial_num)))
-prime_factors = []
+list_of_palindromes = []
+def palindrome_check(num):
+    flipped_str_list = []
+    str_num = str(num)
+    letters_in_str = list(str_num)
+    for num in range (0, len(letters_in_str)):
+        flipped_str_list.append(letters_in_str[-1])
+        letters_in_str = letters_in_str[:-1]
+    flipped_str = "".join(flipped_str_list)
+    if flipped_str == str_num:
+        palindrome = True
+    else:
+        palindrome = False
+    return palindrome
 
-for num in range (2, sq_root_of_initial):
-    if new_num % num == 0:
-        new_num = int(new_num/num)
-        prime_factors.append(num)
+def find_biggest_num(list):
+    while len(list) > 1:
+        if list[0] < list[1]:
+            list.pop(0)
+        else:
+            list.pop(1)
+    return list
 
+for num1 in range (100, 1000):
+    for num2 in range (100,1000):
+        product = num1*num2
+        if palindrome_check(product) == True:
+            list_of_palindromes.append(product)
+print(find_biggest_num(list_of_palindromes)[0])
 
-print(prime_factors[-1])
