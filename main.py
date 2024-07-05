c117dc5cff5e0b7a224ae1d5ec580956358ec8bf
RafeElliff <rafe.elliff@westminster.org.uk>
2024-07-05 13:12:54 +0100

    Problem 2

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+num1 = 1
+num2 = 2
+list_of_terms = [1, 2]
+while num2 < 4000000:
+    temp_num = num1 + num2
+    num1 = num2
+    num2 = temp_num
+    list_of_terms.append(num2)
+print(list_of_terms)
+
 total = 0
-for num in range (1, 1000):
-    if num % 3 == 0:
-        print(num)
-        total = total + num
-    elif num % 5 == 0:
-        print(num)
+for num in list_of_terms:
+    if num % 2 == 0:
         total = total + num
 print (total)
