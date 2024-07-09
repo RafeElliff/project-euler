766f7ac19e7bad931bfcc048de523754c11b4029
RafeElliff <rafe.elliff@westminster.org.uk>
2024-07-09 13:29:23 +0100

    Problem 10

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import math
-for a in range (1, 1001):
-    for b in range (1, 1001):
-        c =math.sqrt(a**2 + b**2)
-        if a + b + c == 1000:
-            print(a*b*c)
+
+list_of_primes = []
+
+#reused code from problem 7
+def is_prime(number_to_check):
+    factor = 2
+    prime = True
+    if number_to_check == 1:
+        prime = False
+    while math.sqrt(number_to_check) >= factor and prime is True:
+        if number_to_check % factor == 0:
+            prime = False
+        else:
+            factor = factor + 1
+    return prime
+
+
+for number in range (1,2000001):
+    if is_prime(number) is True:
+        list_of_primes.append(number)
+
+total = 0
+for number in list_of_primes:
+    total = total+number
+print(total)
