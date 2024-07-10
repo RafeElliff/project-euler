39de8a26be45021ad15d572001b47a0e10926348
RafeElliff <rafe.elliff@westminster.org.uk>
2024-07-10 14:54:00 +0100

    Problem 11

--- main.py
+++ main.py
@@ -1,26 +1,74 @@
-import math
 
-list_of_primes = []
-
-#reused code from problem 7
-def is_prime(number_to_check):
-    factor = 2
-    prime = True
-    if number_to_check == 1:
-        prime = False
-    while math.sqrt(number_to_check) >= factor and prime is True:
-        if number_to_check % factor == 0:
-            prime = False
-        else:
-            factor = factor + 1
-    return prime
+#all of this code was only used to construct the 2d array
+string= "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
+def construct_array(string):
+    list_of_nums = string.split(" ")
+    print("[")
+    for c in range (0, 20):
+        row = []
+        for r in range (0, 20):
+            row.append(int(list_of_nums[r]))
+        list_of_nums = list_of_nums[20:]
+        print(row)
+    print("]")
 
 
-for number in range (1,2000001):
-    if is_prime(number) is True:
-        list_of_primes.append(number)
+matrix = [
+[8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
+[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
+[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+[52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
+[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+[24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+[67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
+[24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+[21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
+[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
+[16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+[86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+[19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+[4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+[88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+[4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
+[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
+[20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+[1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+]
 
-total = 0
-for number in list_of_primes:
-    total = total+number
-print(total)
+
+#in this function, I check the product of the 4 numbers in any 4 directions, represented by compass points.
+# These 4 directions are S, E, NE and SE. As multiplication is commutative
+# there is no need to check any directions because they all mirror the points given
+#Also note that the Y coordinate is flipped, i.e. the lowest co-ordinate is at the top
+
+def check_point(position, matrix):
+    list_of_products = []
+    parts_of_pos = position.split(",")
+    x_coord_of_position = int(parts_of_pos[0])
+    y_coord_of_position = int(parts_of_pos[1])
+    if x_coord_of_position + 3 < 20 and y_coord_of_position + 3 < 20:
+        product_SE = matrix[x_coord_of_position][y_coord_of_position] * matrix[x_coord_of_position+1][y_coord_of_position+1] * matrix[x_coord_of_position+2][y_coord_of_position+2] * matrix[x_coord_of_position+3][y_coord_of_position+3]
+        list_of_products.append(product_SE)
+    if x_coord_of_position + 3 < 20 and y_coord_of_position - 3 > 0:
+        product_NE = matrix[x_coord_of_position][y_coord_of_position] * matrix[x_coord_of_position+1][y_coord_of_position-1] * matrix[x_coord_of_position+2][y_coord_of_position-2] * matrix[x_coord_of_position+3][y_coord_of_position-3]
+        list_of_products.append(product_NE)
+    if x_coord_of_position + 3 < 20:
+        product_S = matrix[x_coord_of_position][y_coord_of_position] * matrix[x_coord_of_position+1][y_coord_of_position] * matrix[x_coord_of_position+2][y_coord_of_position] * matrix[x_coord_of_position+3][y_coord_of_position]
+        list_of_products.append(product_S)
+    if y_coord_of_position + 3 < 20:
+        product_E = matrix[x_coord_of_position][y_coord_of_position] * matrix[x_coord_of_position][y_coord_of_position+1] * matrix[x_coord_of_position][y_coord_of_position+2] * matrix[x_coord_of_position][y_coord_of_position+3]
+        list_of_products.append(product_E)
+    if list_of_products:
+        return max(list_of_products)
+    else:
+        return 0
+
+
+current_highest = 0
+for y_coord in range (0, len(matrix)):
+    for x_coord in range (0, len(matrix[0])):
+        coordinate = str(x_coord) + "," + str(y_coord)
+        if current_highest < check_point(coordinate, matrix):
+            current_highest = check_point(coordinate, matrix)
+print(current_highest)
