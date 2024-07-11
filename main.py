4905a849f0757597d88c9da5d93d60fba57468d2
RafeElliff <rafe.elliff@westminster.org.uk>
2024-07-11 11:51:49 +0100

    Problem 16

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-#moves right and down
-#2 moves available as long as not on furthest right or furthest bottom tile
-# Always takes exactly 40 moves to get to end
-#therefore 40! possible routes
-#as each set of moves has 20 rights and 20 downs, which can be in any order, there will be duplicated. These can be eliminated by dividing our answer(20!)*(20!)
+list_of_digits = list(str(2**1000))
+print(list_of_digits)
+total = 0
+for num in list_of_digits:
+    total = total + int(num)
 
-import math
-print(math.factorial(40)/math.factorial(20)**2)
-#not a lot of code for this one, difficult part was working out the actual maths
+print(total)
