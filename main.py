bb53f0d2e7f0548b8e88508d89dc87c90389010d
RafeElliff <rafe.elliff@westminster.org.uk>
2024-07-11 11:49:21 +0100

    Problem 15

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-def collatz_sequence(number):
-    num_of_terms = 0
-    while number != 1:
-        num_of_terms = num_of_terms + 1
-        if number % 2 == 0:
-            number = number / 2
-        else:
-            number = (number * 3) + 1
-    num_of_terms = num_of_terms + 1
-    return num_of_terms
+#moves right and down
+#2 moves available as long as not on furthest right or furthest bottom tile
+# Always takes exactly 40 moves to get to end
+#therefore 40! possible routes
+#as each set of moves has 20 rights and 20 downs, which can be in any order, there will be duplicated. These can be eliminated by dividing our answer(20!)*(20!)
 
-
-current_highest = 0
-for num in range(1, 1000001):
-    if collatz_sequence(num) > current_highest:
-        current_highest = collatz_sequence(num)
-        final_answer = num
-print(final_answer)
-
-
-
+import math
+print(math.factorial(40)/math.factorial(20)**2)
+#not a lot of code for this one, difficult part was working out the actual maths
