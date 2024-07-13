697ae649655682becde7e2df132c736d6520f4ea
RafeElliff <rafe.elliff@westminster.org.uk>
2024-07-13 15:15:44 +0100

    Problem 21

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-def factorial(number):
-    total = 1
-    for num in range(1, number+1):
-        total = total * num
+def function_d(number):
+    factors = []
+    for counter in range (1, number):
+        if number % counter == 0:
+            factors.append(counter)
+    total = 0
+    for factor in factors:
+        total = total + factor
+
     return total
 
-list = list(str(factorial(100)))
+amicable_numbers = []
+for num_a in range (1, 10001):
+    num_b = function_d(num_a)
+    if function_d(num_b) == num_a and num_a != num_b:
+        if num_a not in amicable_numbers:
+            amicable_numbers.append(num_a)
+            amicable_numbers.append(num_b)
+print(amicable_numbers)
 total = 0
-for number in list:
-    total = total + int(number)
-print (total)
+for number in amicable_numbers:
+    total = total + number
+print(total)
